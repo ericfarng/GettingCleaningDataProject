333f4fccbc96359792aa85943fd1df88f6f1e3eb
Eric Farng <efarng@grubhub.com>
2014-11-20 11:44:38 -0500

    adding R script

---
+++ run_analysis.R
@@ -0,0 +1,62 @@
+workingDirectory = "~/coursera/Getting and Cleaning Data/Assignment 2"
+dir.create(file.path(workingDirectory), showWarnings = FALSE)
+setwd(workingDirectory)
+
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
+              destfile=paste(workingDirectory, "/Dataset.zip", sep=""), method="wget")
+
+library(utils)
+unzip(paste(workingDirectory, "/Dataset.zip", sep=""))
+
+dataDir = paste(workingDirectory, "/UCI HAR Dataset/", sep="")
+trainDir = paste(workingDirectory, "/UCI HAR Dataset/train/", sep="")
+testDir = paste(workingDirectory, "/UCI HAR Dataset/test/", sep="")
+allDir = paste(workingDirectory, "/UCI HAR Dataset/all/", sep="")
+
+dir.create(file.path(allDir), showWarnings = FALSE)
+dir.create(file.path(paste(allDir, "/Inertial Signals", sep="")), showWarnings = FALSE)
+
+combineFile <- function(filename) {
+    allFilename = gsub("train", "all", filename)
+    outfile <- file(paste(allDir, allFilename,sep=""), open="w+")
+    
+    dat <- readLines(paste(trainDir, filename, sep=""))
+    dat <- lapply(dat, function(x) {paste("train", x, sep=" ")})
+    dat <- gsub("  ", " ", dat)
+    dat <- gsub("  ", " ", dat)
+    writeLines(as.character(dat),outfile)
+    
+    testFilename = gsub("train", "test", filename)
+    dat <- readLines(paste(testDir, testFilename, sep=""))
+    dat <- lapply(dat, function(x) {paste("test", x, sep=" ")})
+    dat <- gsub("  ", " ", dat)
+    dat <- gsub("  ", " ", dat)
+    writeLines(as.character(dat),outfile)
+    
+    close(outfile)
+    allFilename
+}
+
+trainFiles <- list.files(trainDir, recursive=TRUE)
+fileNames <- sapply(trainFiles, combineFile)
+
+activity <- read.table(paste(allDir, "y_all.txt",sep=""))
+subject <- read.table(paste(allDir, "subject_all.txt",sep=""))
+feature <- read.table(paste(allDir, "x_all.txt",sep=""))
+featureLabel <- read.table(paste(dataDir, "features.txt", sep=""))
+
+activityLabel <- read.table(paste(dataDir, "activity_labels.txt", sep=""))
+activityLabelled <- sapply(activity[,2], function(x) {as.character(activityLabel[x, 2])})
+
+names(feature) <- c("Data Set", as.character(featureLabel[,2]))
+names(subject) <- c("Data Set", "Subject")
+
+dataSet <- cbind(subject, activityLabelled, feature[,grep("(mean|std)", names(feature))])
+names(dataSet)[3] = "Activity"
+
+head(dataSet[,1:10])
+
+library(reshape2)
+melted <- melt(dataSet, id=c("Activity", "Subject"), measure.vars=names(dataSet[4:ncol(dataSet)]))
+dcasted <- dcast(melted, Activity + Subject ~ variable, mean)
+write.table(dcasted, file=paste(workingDirectory, "/measurementMean.txt", sep=""), row.names=FALSE)
